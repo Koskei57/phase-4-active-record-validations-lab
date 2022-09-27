da8018489138a6048120227bd343a1a9cf2bfc6d
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-27 15:33:13 +0300

    commit tree a578d43969f6af63ba572f9e81612dc7f7babac1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+  #   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} shoulda-matchers test string" }
+
 end
